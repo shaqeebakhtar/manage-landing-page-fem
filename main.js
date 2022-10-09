8ea72c07ec54e9555f6fffed62737b05711fe253
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2022-10-09 13:43:59 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
+
+// slider
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false,
+    },
+  },
+});
